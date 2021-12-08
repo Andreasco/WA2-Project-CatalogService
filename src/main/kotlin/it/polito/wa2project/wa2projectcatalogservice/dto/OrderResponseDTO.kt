c6b521c779e29d645e0da17e6ce915ce9fa05c20
Guido R <guidonguido@gmail.com>
2021-12-08 09:51:02 +0100

    Add props on OrderResponseDTO

--- src/main/kotlin/it/polito/wa2project/wa2projectcatalogservice/dto/OrderResponseDTO.kt
+++ src/main/kotlin/it/polito/wa2project/wa2projectcatalogservice/dto/OrderResponseDTO.kt
@@ -1,5 +1,7 @@
 package it.polito.wa2project.wa2projectcatalogservice.dto
 
 data class OrderResponseDTO(
-    val orderId: Long
+    val orderId: Long?,
+    val uuid: String,
+    val exitStatus: Long
 )
