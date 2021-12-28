886ebdef2f04bf136254397c59510555a6f86c7f
Andrea Scoppetta <andre.sco@icloud.com>
2021-12-28 13:06:38 +0100

    Implement wallet endpoints

--- src/main/kotlin/it/polito/wa2project/wa2projectcatalogservice/controllers/WalletController.kt
+++ src/main/kotlin/it/polito/wa2project/wa2projectcatalogservice/controllers/WalletController.kt
@@ -1,27 +1,73 @@
 package it.polito.wa2project.wa2projectcatalogservice.controllers
 
+import it.polito.wa2project.wa2projectcatalogservice.services.restServices.WalletRestService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 
 @RestController
-@RequestMapping("/wallet")
+@RequestMapping("/wallets")
 @Validated
-class WalletController {
+class WalletController(val walletRestService: WalletRestService) {
 
-    /*//TODO come faccio per prenderne più di uno? Lista di ID?
     @GetMapping("/{walletId}")
     fun getWallet(
         @PathVariable
         @Positive(message = "Insert a valid walletId")
         walletId: Long
-    ): ResponseEntity<WalletDTO> {
+    ): ResponseEntity<String> {
+        val response = walletRestService.getWallet(walletId)
 
-        return ResponseEntity(walletDTO, HttpStatus.OK)
-    }*/
+        return ResponseEntity(response, HttpStatus.OK)
+    }
+
+    @PostMapping("/{walletId}")
+    fun addWallet(
+        @PathVariable
+        @Positive(message = "Insert a valid walletId")
+        walletId: Long
+    ): ResponseEntity<String> {
+        val response = walletRestService.addWallet()
+
+        return ResponseEntity(response, HttpStatus.OK)
+    }
+
+    @GetMapping("/{walletId}/transactions")
+    fun getTransactionsBetweenDate(
+        @PathVariable
+        @Positive(message = "Insert a valid walletId")
+        walletId: Long,
+
+        @RequestParam
+        @NotNull(message = "'from' timestamp is required")
+        @Positive(message = "Insert a valid 'from' timestamp")
+        from: Long? = null,
+
+        @RequestParam
+        @NotNull(message = "'to' timestamp is required")
+        @Positive(message = "Insert a valid 'to' timestamp")
+        to: Long? = null
+    ): ResponseEntity<String>{
+        val setOfTransaction = walletRestService.getTransactionsBetweenDate(walletId, from!!, to!!)
+
+        return ResponseEntity(setOfTransaction, HttpStatus.OK)
+    }
+
+    @GetMapping("{walletId}/transactions/{transactionId}")
+    fun getTransaction(
+        @PathVariable
+        @Positive(message = "Insert a valid walletId")
+        walletId: Long,
+
+        @PathVariable
+        @Positive(message = "Insert a valid transactionId")
+        transactionId: Long,
+    ): ResponseEntity<String>{
+        val response = walletRestService.getTransaction(walletId, transactionId)
+
+        return ResponseEntity(response, HttpStatus.OK)
+    }
 }
---
+++ src/main/kotlin/it/polito/wa2project/wa2projectcatalogservice/services/restServices/WalletRestService.kt
@@ -0,0 +1,85 @@
+package it.polito.wa2project.wa2projectcatalogservice.services.restServices
+
+import it.polito.wa2project.wa2projectcatalogservice.dto.warehouse.ProductDTO
+import it.polito.wa2project.wa2projectcatalogservice.repositories.UserRepository
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForObject
+import org.springframework.web.client.postForObject
+import java.util.*
+
+@Service
+class WalletRestService(restTemplateBuilder: RestTemplateBuilder, userRepository: UserRepository) {
+    private val restTemplate: RestTemplate
+
+    private val walletServiceURL = "http://localhost:8200/walletService/wallets" //TODO cambia la porta?
+    private val userId: Long
+
+    init {
+        restTemplate = restTemplateBuilder.build()
+        val usernameLogged = SecurityContextHolder.getContext().authentication.principal as String
+        userId = userRepository.findByUsername(usernameLogged)!!.getId()!!
+    }
+
+    //TODO aggiungo anche lo userId per sicurezza?
+    fun getWallet(walletId: Long): String{
+        val url = "$walletServiceURL/$walletId"
+
+        val response: String = restTemplate.getForObject(url) //Dovrebbe contenere il JSON che mi manda wallet service
+
+        println("GET WALLET: Wallet service response $response")
+
+        return response
+    }
+
+    fun addWallet(): String{
+        //Create headers
+        val headers = HttpHeaders()
+
+        //Set `content-type` header
+        headers.contentType = MediaType.APPLICATION_JSON
+
+        //Set `accept` header
+        headers.accept = Collections.singletonList(MediaType.APPLICATION_JSON)
+
+        //Create a map for post parameters
+        val map: MutableMap<String, Any> = HashMap()
+        map["userId"] = userId
+
+        //Build the request
+        val entity = HttpEntity(map, headers)
+
+        //Send POST request
+        val response: String = restTemplate.postForObject(walletServiceURL, entity)
+
+        println("ADD WALLET: Wallet service response $response")
+
+        return response
+    }
+
+    //TODO aggiungo anche l'userId per essere sicuro di chi richiede le transazioni?
+    fun getTransactionsBetweenDate(walletId: Long, from: Long, to: Long): String{
+        val url = "$walletServiceURL/$walletId/transactions?from=$from&to=$to"
+
+        val response: String = restTemplate.getForObject(url) //Dovrebbe contenere il JSON che mi manda wallet service
+
+        println("GET TRANSACTIONS BETWEEN DATES: Wallet service response $response")
+
+        return response
+    }
+
+    fun getTransaction(walletId: Long, transactionId: Long): String{
+        val url = "$walletServiceURL/$walletId/transactions/$transactionId"
+
+        val response: String = restTemplate.getForObject(url) //Dovrebbe contenere il JSON che mi manda wallet service
+
+        println("GET TRANSACTION: Wallet service response $response")
+
+        return response
+    }
+}
