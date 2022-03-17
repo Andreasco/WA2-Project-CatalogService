a796a700352112d6bd2500eb7c20f8b7d694b64d
Andrea Scoppetta <andre.sco@icloud.com>
2022-03-17 10:49:17 +0100

    Add customerId to UserPublicDTO

--- src/main/kotlin/it/polito/wa2project/wa2projectcatalogservice/dto/auth/UserPublicDTO.kt
+++ src/main/kotlin/it/polito/wa2project/wa2projectcatalogservice/dto/auth/UserPublicDTO.kt
@@ -4,6 +4,7 @@ import it.polito.wa2project.wa2projectcatalogservice.domain.User
 
 data class UserPublicDTO(
     var username: String?,
+    var customerId: Long,
     var email: String?,
     var isEnabled: Boolean?,
     var roles: String?
@@ -11,6 +12,7 @@ data class UserPublicDTO(
 
 fun User.toUserPublicDTO() = UserPublicDTO(
     username,
+    customer.getId()!!,
     email,
     isEnabled,
     roles
